d4ff0ce287fca6009bdc82392a71abf8693c7eb0
Xander3001 <100565653+Xander3001@users.noreply.github.com>
2023-01-22 14:49:41 +0000

    Update Digit-Recognizer.py

--- Digit-Recognizer/Digit-Recognizer.py
+++ Digit-Recognizer/Digit-Recognizer.py
@@ -1,37 +1,3 @@
-import numpy as np
-import input_data
-import cv2
-import Digit_Recognizer_DL
-import Digit_Recognizer_LR
-import Digit_Recognizer_NN
-
-
-def main():
-    mnist = input_data.read_data_sets("MNIST_data/", one_hot=False)
-    data = mnist.train.next_batch(8000)
-    train_x = data[0]
-    Y = data[1]
-    train_y = (np.arange(np.max(Y) + 1) == Y[:, None]).astype(int)
-    mnist = input_data.read_data_sets("MNIST_data/", one_hot=False)
-    tb = mnist.train.next_batch(2000)
-    Y_test = tb[1]
-    X_test = tb[0]
-    # 0.00002-92
-    # 0.000005-92, 93 when 200000 190500
-
-    d1 = Digit_Recognizer_LR.model(train_x.T, train_y.T, Y, X_test.T, Y_test, num_iters=1500, alpha=0.05,
-                                   print_cost=True)
-    w_LR = d1["w"]
-    b_LR = d1["b"]
-
-    d2 = Digit_Recognizer_NN.model_nn(train_x.T, train_y.T, Y, X_test.T, Y_test, n_h=100, num_iters=1500, alpha=0.05,
-                                      print_cost=True)
-
-    dims = [784, 100, 80, 50, 10]
-    d3 = Digit_Recognizer_DL.model_DL(train_x.T, train_y.T, Y, X_test.T, Y_test, dims, alpha=0.5, num_iterations=1100,
-                                      print_cost=True)
-
-    cap = cv2.VideoCapture(0)
 
     while (cap.isOpened()):
         ret, img = cap.read()
@@ -67,16 +33,3 @@ def main():
         k = cv2.waitKey(10)
         if k == 27:
             break
-
-
-def get_img_contour_thresh(img):
-    x, y, w, h = 0, 0, 300, 300
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    blur = cv2.GaussianBlur(gray, (35, 35), 0)
-    ret, thresh1 = cv2.threshold(blur, 70, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
-    thresh1 = thresh1[y:y + h, x:x + w]
-    contours, hierarchy = cv2.findContours(thresh1, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[-2:]
-    return img, contours, thresh1
-
-
-main()
